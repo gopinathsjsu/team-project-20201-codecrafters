776cdc196f43df63cfda770c9304ad192ed5d7dd
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2025-03-08 01:36:52 -0800

    Added Layout component with navigation links for Login and Signup and makes use of Outlet and Link from react-router-dom to render child components.

---
+++ cmpe202project/src/pages/Layout.jsx
@@ -0,0 +1,30 @@
+import { Outlet, Link } from "react-router-dom";
+
+const Layout = () => {
+  return (
+    <>
+      <nav>
+        <ul
+          style={{
+            display: "flex",
+            gap: "2rem",
+            marginBottom: "4rem",
+            justifySelf: "center",
+            listStyleType: "none",
+          }}
+        >
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  );
+};
+
+export default Layout;
