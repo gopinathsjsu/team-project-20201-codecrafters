57a0a3eef18eaff661ccbe2457e33e33a54d7668
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2025-03-08 01:33:10 -0800

    Implemented routing with Login, Sign Up, and moved LoginForm and SignUpForm to a components folder for better organization.

--- cmpe202project/src/App.jsx
+++ cmpe202project/src/App.jsx
@@ -1,10 +1,21 @@
-import AuthForm from "./AuthForm";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
+import Layout from "./pages/Layout";
+import NoPage from "./pages/NoPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <AuthForm />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
