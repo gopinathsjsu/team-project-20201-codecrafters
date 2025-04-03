76f14421a0284e7e98e3857d6eb62f6cc632f97a
Khanh Chung <khanh.d.chung@gmail.com>
2025-04-02 22:27:52 -0700

    implement reservation system

--- server/src/main/java/com/example/server/dto/reservation/ReservationUpdateDTO.java
+++ server/src/main/java/com/example/server/dto/reservation/ReservationUpdateDTO.java
@@ -1,5 +1,6 @@
 package com.example.server.dto.reservation;
 
+import com.example.server.entity.ReservationStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,8 +11,9 @@ import java.time.LocalTime;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ReservationCreateDTO {
+public class ReservationUpdateDTO {
     private LocalDate date;
     private LocalTime time;
     private int partySize;
+    private ReservationStatus status;
 }
