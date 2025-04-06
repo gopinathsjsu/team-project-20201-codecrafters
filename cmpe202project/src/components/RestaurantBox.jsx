66590cb0b239a2bc1e0309047cd6a6a9dc4b94f9
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2025-04-05 18:29:52 -0700

    Added TrendUp icon and update RestaurantBox component for dynamic data display

--- cmpe202project/src/components/RestaurantBox.jsx
+++ cmpe202project/src/components/RestaurantBox.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "../styles/RestaurantBox.css";
 import RestaurantImage from "../assets/restaurant-example.png";
+import TrendUp from "../assets/trend-up.svg";
 
-const RestaurantBox = () => {
+const RestaurantBox = ({
+  name,
+  rating,
+  reviews,
+  cuisine,
+  bookedTimes,
+  timeSlots,
+}) => {
   return (
     <div className="restaurant-box">
       <div className="image-container">
@@ -13,23 +21,38 @@ const RestaurantBox = () => {
         />
       </div>
       <div className="restaurant-info">
-        <h2 className="restaurant-name">Fina Ristorante</h2>
+        <h2 className="restaurant-name">{name}</h2>
         <div className="rating-container">
           <div className="star-rating">
+            <span className="rating-value">{rating} </span>
             <span className="star">&#9733;</span> {/* Star 1 */}
             <span className="star">&#9733;</span> {/* Star 2 */}
             <span className="star">&#9733;</span> {/* Star 3 */}
             <span className="star">&#9733;</span> {/* Star 4 */}
             <span className="star">&#9734;</span> {/* Star 5 (empty) */}
           </div>
-          <div className="reviews">2,100 reviews</div>
+          <div className="reviews">{reviews} reviews</div>
         </div>
-        <p className="restaurant-cuisine">Italian • Seafood • Mediterranean</p>
-        <p className="booked-times">Booked 15 times today</p>
+        <p className="restaurant-cuisine">{cuisine}</p>
+        <div className="booked-times">
+          <img src={TrendUp} alt="Trend Up" className="trend-icon" />
+          Booked {bookedTimes} times today
+        </div>
+        {timeSlots && timeSlots.length > 0 ? (
+          <span className="availability-text">Available Time Slots:</span>
+        ) : (
+          <span className="availability-text">No available time slots</span>
+        )}
         <div className="availability">
-          <span className="time-slot">6:30 PM</span>
-          <span className="time-slot">7:00 PM</span>
-          <span className="time-slot">8:00 PM</span>
+          {timeSlots && timeSlots.length > 0 ? (
+            timeSlots.map((time, index) => (
+              <span key={index} className="time-slot">
+                {time}
+              </span>
+            ))
+          ) : (
+            <span className="no-availability">No slots available</span>
+          )}
         </div>
       </div>
     </div>
