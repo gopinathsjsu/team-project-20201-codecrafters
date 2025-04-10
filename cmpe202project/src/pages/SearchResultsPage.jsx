923c9be4288a498e736b56f54bbd555bc318cb3a
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2025-04-10 15:43:41 -0700

    Refactor SearchResultsPage to simplify reservation details display and enhance SearchComponent integration

--- cmpe202project/src/pages/SearchResultsPage.jsx
+++ cmpe202project/src/pages/SearchResultsPage.jsx
@@ -1,29 +1,16 @@
 import React, { useContext, useEffect } from "react";
 import { ReservationContext } from "../context/ReservationContext";
 import SearchComponent from "../components/SearchComponent";
+import "../styles/SearchResultsPage.css";
 
 const SearchResultsPage = () => {
   const { reservationDate, reservationTime, numberOfGuests, searchTerm } =
     useContext(ReservationContext);
 
-  useEffect(() => {
-    console.log("Reservation Info:", {
-      reservationDate,
-      reservationTime,
-      numberOfGuests,
-      searchTerm,
-    });
-  }, [reservationDate, reservationTime, numberOfGuests, searchTerm]);
-
   return (
     <main className="search-container">
-      <SearchComponent />
-      <div className="reservation-info">
-        <h2>Reservation Details</h2>
-        <p>Date: {reservationDate || "Not provided"}</p>
-        <p>Time: {reservationTime || "Not provided"}</p>
-        <p>Guests: {numberOfGuests || "Not provided"}</p>
-        <p>Search Term: {searchTerm || "Not provided"}</p>
+      <div className="search-component-container">
+        <SearchComponent horizontal={true} />
       </div>
       <div className="search-results">
         <div className="filters"></div>
