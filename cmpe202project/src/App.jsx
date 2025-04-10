5b08917b491b8bce6fe9d0f49dae68162b314f6b
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2025-04-10 15:05:59 -0700

    Refactor routing structure to include search results page and clean up admin routes

--- cmpe202project/src/App.jsx
+++ cmpe202project/src/App.jsx
@@ -5,7 +5,7 @@ import ApproveNewRestaurant from "./AdminAddRest/ApproveNewRestauant";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import CustomerLayout from "./pages/CustomerLayout";
-import AdminLayout from "./pages/AdminLayout";
+import SearchResultsPage from "./pages/SearchResultsPage";
 import NoPage from "./pages/NoPage";
 import HomePage from "./pages/HomePage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -15,15 +15,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<CustomerLayout/>}>
+        <Route path="/" element={<CustomerLayout />}>
           <Route index path="/" element={<HomePage />} />
+          <Route path="search" element={<SearchResultsPage />} />
         </Route>
         <Route path="/" element={<Layout />}>
           <Route path="login" element={<LoginPage />} />
           <Route path="signup" element={<SignUpPage />} />
-          <Route path="/admin/dashboard" element={<Dashboard />} />
-          <Route path="/admin/approve" element={<ApproveNewRestaurant />} />
-          <Route path="/admin/restaurants" element={<RemoveRest />} />
+          <Route path="admin/dashboard" element={<Dashboard />} />
+          <Route path="admin/approve" element={<ApproveNewRestaurant />} />
+          <Route path="admin/restaurants" element={<RemoveRest />} />
         </Route>
         <Route path="*" element={<NoPage />} />
       </Routes>
