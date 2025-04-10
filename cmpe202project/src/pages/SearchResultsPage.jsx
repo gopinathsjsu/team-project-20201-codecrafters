8531b8ba0328f6e0e9321a0647e24ffd2bf2ae66
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2025-04-10 16:13:54 -0700

    Refactor SearchResultsPage to enhance filter options and improve layout styling

--- cmpe202project/src/pages/SearchResultsPage.jsx
+++ cmpe202project/src/pages/SearchResultsPage.jsx
@@ -12,9 +12,81 @@ const SearchResultsPage = () => {
       <div className="search-component-container">
         <SearchComponent horizontal={true} />
       </div>
-      <div className="search-results">
-        <div className="filters"></div>
-        <div className="results"></div>
+      <div className="search-results-container">
+        <div className="filters-container">
+          <ul className="filter-item">
+            <li className="filter-type">Type of Cuisine</li>
+            <label>
+              <input type="checkbox" value="Italian" /> Italian
+            </label>
+            <label>
+              <input type="checkbox" value="Chinese" /> Chinese
+            </label>
+            <label>
+              <input type="checkbox" value="Indian" /> Indian
+            </label>
+            <label>
+              <input type="checkbox" value="Mexican" /> Mexican
+            </label>
+            <label>
+              <input type="checkbox" value="American" /> American
+            </label>
+          </ul>
+          <div className="filter-item">
+            <li className="filter-type">Price</li>
+            <label>
+              <input type="checkbox" value="$" /> $
+            </label>
+            <label>
+              <input type="checkbox" value="$$" /> $$
+            </label>
+            <label>
+              <input type="checkbox" value="$$$" /> $$$
+            </label>
+            <label>
+              <input type="checkbox" value="$$$$" /> $$$$
+            </label>
+          </div>
+          <div className="filter-item">
+            <li className="filter-type">Rating</li>
+            <label>
+              <input type="checkbox" value="1" />
+              <span>⭐</span>
+            </label>
+            <label>
+              <input type="checkbox" value="2" />
+              <span>⭐⭐</span>
+            </label>
+            <label>
+              <input type="checkbox" value="3" />
+              <span>⭐⭐⭐</span>
+            </label>
+            <label>
+              <input type="checkbox" value="4" />
+              <span>⭐⭐⭐⭐</span>
+            </label>
+            <label>
+              <input type="checkbox" value="5" />
+              <span>⭐⭐⭐⭐⭐</span>
+            </label>
+          </div>
+          <div className="filter-item">
+            <li className="filter-type">Location</li>
+            <label>
+              <input type="checkbox" value="New York" /> New York
+            </label>
+            <label>
+              <input type="checkbox" value="San Francisco" /> San Francisco
+            </label>
+            <label>
+              <input type="checkbox" value="Los Angeles" /> Los Angeles
+            </label>
+            <label>
+              <input type="checkbox" value="Chicago" /> Chicago
+            </label>
+          </div>
+        </div>
+        <div className="results-container">Results</div>
       </div>
     </main>
   );
