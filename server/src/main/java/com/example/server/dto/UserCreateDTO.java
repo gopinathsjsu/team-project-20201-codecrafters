26008cec2c2b9c2b29e6b109379e579562b17f44
Khanh Chung <khanh.d.chung@gmail.com>
2025-04-18 10:43:35 -0700

    Add more test cases to AuthControllerTest

--- server/src/main/java/com/example/server/dto/UserCreateDTO.java
+++ server/src/main/java/com/example/server/dto/UserCreateDTO.java
@@ -1,6 +1,8 @@
 package com.example.server.dto;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,10 +13,16 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserCreateDTO {
+
     @NotBlank(message = "Email cannot be blanked")
+    @Email
     private String email;
+
     @NotBlank(message = "Password cannot be blanked")
+    @Size(min = 8, message = "{validation.name.size.too_short}")
+    @Size(max = 16, message = "{validation.name.size.too_long}")
     private String password;
+
     @NotBlank(message = "Phone cannot be blanked")
     private String phone;
     private Set<String> roles;
