4ffa8a84803c6a69598274fee35f5fc62fc32da3
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2025-04-19 19:14:39 -0700

    Remove unnecessary whitespace in App component for cleaner code

--- cmpe202project/src/App.jsx
+++ cmpe202project/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
           <Route path="admin/restaurants" element={<RemoveRest />} />
         </Route>
         <Route path="*" element={<NoPage />} />
-      </Routes>{" "}
+      </Routes>
     </BrowserRouter>
   );
 }
