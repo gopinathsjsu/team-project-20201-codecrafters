412b8d03fd0ef8ff4bd1a15e3ed6a657ea0df1b2
Miguel Vazquez <98563210+MiguelVazB@users.noreply.github.com>
2025-04-24 14:43:13 -0700

    Comment out search bar in CustomerNavBar for future implementation

--- cmpe202project/src/components/CustomerNavBar.jsx
+++ cmpe202project/src/components/CustomerNavBar.jsx
@@ -12,11 +12,11 @@ const CustomerNavBar = () => {
         Logo
       </div>
       <div className="search-login-container">
-        <div className="search-bar">
+        {/* <div className="search-bar">
           <img src={searchIcon} alt="search icon" />
           <input type="text" placeholder="Search..." className="search-input" />
           <button className="search-button">Search</button>
-        </div>
+        </div> */}
         <button className="login-btn" onClick={() => navigate("/login")}>
           Login
         </button>
